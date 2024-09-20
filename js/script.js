58885b77a7feced1d8ce6f6785d4afb083ec480f
jared hales <jared.hales2838@STU.MTEC.EDU>
2024-09-20 09:00:18 -0600

    struggling to make this work

--- js/script.js
+++ js/script.js
@@ -56,45 +56,38 @@ function newActivity() {
   }
 }
 function newList() {
-  var header = document.createElement("container");
-  newDiv.classList.add('myClass', 'anotherClass');
-  var header = document.createElement("div");
-  newDiv.classList.add('myClass', 'anotherClass');
+  var list = document.createElement("div");
+  list.classList.add('container');
+
+  var headerText = document.createElement("div");
+  headerText.classList.add('header');
+
   var hText = document.createElement("h2");
-  newDiv.classList.add('myClass', 'anotherClass');
+  hText.classList.add('marge');
+
   var titleInput = document.createElement("input");
-  newDiv.classList.add('myClass', 'anotherClass');
+  titleInput.classList.add('myClass');
+
   var fakeBtn = document.createElement("span");
-  newDiv.classList.add('myClass', 'anotherClass');
-  var listInput = document.getElementById("listInput").value;
-  newDiv.classList.add('myClass', 'anotherClass');
+  fakeBtn.classList.add('addBtn');
+
+  var listValue = document.getElementById("listInput").value;
   var l = document.createTextNode(listValue);
 
-  header.appendChild(l);
-  if (listValue === '') {
-    alert("You must write something!");
-  } else if (listValue === ' ') {
-    alert("You must write something!");
-  }
-   else {
-    document.getElementById("container").appendChild(header);
-    document.getElementById("container").appendChild(hText);
-    document.getElementById("container").appendChild(titleInput);
-    document.getElementById("container").appendChild(fakeBtn);
-    document.getElementById("container").appendChild(listInput);
-  }
-  document.getElementById("listValue").value = "";
+  list.appendChild(l);
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
+  if (listValue === '' || listValue.trim() === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("containerContainer").appendChild(list);
+    list.appendChild(headerText);
+    list.appendChild(hText);
+    list.appendChild(titleInput);
+    list.appendChild(fakeBtn);
   }
+
+  document.getElementById("listInput").value = "";
+
+
 }
+
